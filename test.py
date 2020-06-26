1acd07c8cf00a930062e4b7808778398d79bb97e
Daniel Elder <danielelder@sdl.com>
2020-06-26 18:00:04 +0900

    add node delete function

--- test.py
+++ test.py
@@ -2,6 +2,9 @@ from sele import make_driver
 from tools import check_for_load
 import os
 import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 url = "https://search.albertare.com/real-estate/calgary_ab"
@@ -21,38 +24,37 @@ with open(property_link_list, 'a', encoding='utf-8') as f:
 
     while True:
         
-        #scroll down to the bottom
+        # scroll down to the bottom
         scroll_height = driver.execute_script("return document.body.scrollHeight;")
         driver.execute_script(f"window.scrollTo(0, {scroll_height});")
-        time.sleep(1)
 
-        #click if 'load more' button appears
+        # click if 'load more' button appears
         load_button = check_for_load(driver)
         if load_button:
             load_button.click()
 
-        #get number of currently loaded entries
-        loaded_properties = driver.find_elements_by_css_selector("div.property")
-        print(f"Properties on the screen: {len(loaded_properties)}")
+        # find the properties available on the screen
+        WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.property > a')))
+        loaded_properties = driver.find_elements_by_css_selector("div.property > a")
+        
+        # get the link of each property and delete the node
         for property in loaded_properties:
-            link = property.find_element_by_css_selector("a").get_attribute('href')
+            # link = property.find_element_by_css_selector("a").get_attribute('href')
+            link = property.get_attribute('href')
             link = link.split('/')[-2:]
             link = '/'.join(link)
+            link = "/" + link
 
-        #delete the nodes to free up memory and maintain scraping speed
-        # for _ in range(len(loaded_properties)):
-        #     driver.execute_script("document.getElementsByClassName('property')[0].remove();")
+            # delete the node
+            script = f"document.querySelector(\"a[href^=\'{link}\']\").remove()"
+            driver.execute_script(script)
+
 
 
 end_time = time.time()
 
 print("Elapsed time: " + end_time - start_time)
 
-# #save the list of property links to csv
-# for property in loaded_properties:
-#     property_link = property.find_element_by_css_selector('a')[0].get_attribute('href')
-#     print(property_link)
-    
-# if property_link not in pulled_list:
-#     pulled_list.append(property_link)
-#     f.write(property_link)
+# successully deleting node
+# make it so that a file is created and the links are saved
+# use proxy or slow scrape to access each site and scrape the info.
